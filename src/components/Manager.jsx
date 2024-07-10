42bb31d22097a6ddf69cc32fb31a4dfe18d6b0a8
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-07-10 20:43:28 +0530

    Save to local storage

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,23 +1,55 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 const Manager = () => {
-  return (
-    <div className="mycontainer bg-red-200">
-        <h1 className='text-4xl'>
-            <span className='text-blue-500'>&lt; </span>
-                Pass
-            <span className='text-blue-500'>OP /&gt;</span>
-        </h1>
-        <p>Your own password manager.</p>
-        <div className='flex flex-col p-4'>
-            <input className='m-3 rounded-full p-2' placeholder='Website' type="text" name='' id=''/>
-            <div className='flex gap-3 m-auto '>
-                <input className='rounded-full p-2' placeholder='username' type="text" name="" id="" />
-                <input className='rounded-full p-2' placeholder='password' type="text" name="" id="" />
+    const [isShown, setIsShown] = useState(true);
+    const [form, setForm] = useState({site: '', username: '', password: ''})
+    const [passwordArray, setPasswordArray] = useState([])
+
+    useEffect(() => {
+      let passwords = localStorage.getItem('passwords');
+      if(passwords){
+        setPasswordArray(JSON.parse(passwords))
+      }
+    }, [])
+    
+
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+    
+    const savePassword = (params) => {
+        setPasswordArray([...passwordArray, form])
+        localStorage.setItem('password', JSON.stringify([...passwordArray, form]))
+        console.log([...passwordArray, form])
+    }
+    
+    const showPassword = (params) => {
+        setIsShown(!isShown);
+    }
+    return (
+        <div className="mycontainer bg-red-50">
+            <h1 className='text-4xl text font-bold text-center'>
+                <span className='text-blue-500'>&lt;</span>
+                    Pass
+                <span className='text-blue-500'>OP/&gt;</span>
+            </h1>
+            <p className='text-blue-900 text-lg font-bold text-center'>Your own password manager.</p>
+            <div className='flex flex-col p-4 gap-8 items-center'>
+                <input value={form.site} onChange={handleChange} className='w-full rounded-full border border-blue-500 p-4 py-1' placeholder='Website' type="text" name='site' id=''/>
+                <div className='flex w-full gap-8 '>
+                    <input value={form.username} onChange={handleChange} className='w-full rounded-full border border-blue-500 p-4 py-1' placeholder='username' type="text" name="username" id="" />
+                    <input value={form.password} onChange={handleChange} className='w-full rounded-full border border-blue-500 p-4 py-1' placeholder='password' type="text" name="password" id="" />
+                    <span className='flex flex-col justify-center items-center cursor-pointer font-bold text-lg' onClick={showPassword}>
+                        <p>
+                            {isShown ? 'Show' : 'Hide'}
+                        </p>
+                    </span>
+                </div>
+                <button onClick={savePassword} className='flex w-fit justify-center items-center border-2 border-blue-800 rounded-full bg-blue-400 p-4 py-2 hover:bg-blue-300 hover:font-bold'>Add Password</button>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Manager
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-red-500 '>
+    <nav className='bg-red-950 '>
         <div className='mycontainer flex justify-between items-center px-4 py-5 h-14'>
             <div className="logo font-bold text-red-50 text-2xl">
-                <span className='text-blue-500'>&lt; </span>
+                <span className='text-blue-500'>&lt;</span>
                 Pass
-                <span className='text-blue-500'>OP /&gt;</span>
+                <span className='text-blue-500'>OP/&gt;</span>
             </div>
             <ul>
-                <li className='flex gap-4'>
+                <li className='flex gap-4 text-red-50'>
                     <a className='hover:font-bold' href="#">Home</a>
                     <a className='hover:font-bold' href="#">About</a>
                     <a className='hover:font-bold' href="#">Contact</a>
